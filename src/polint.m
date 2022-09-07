6713eac88e5e128c38d3c60a1890668a7d93ec29
Cirdans-Home <f.durastante@na.iac.cnr.it>
2022-09-07 17:31:07 +0200

    Fixed indexes

--- src/polint.m
+++ src/polint.m
@@ -8,10 +8,10 @@ p = @(i) sqrt(2*j+1)*(-1)^(j-i)*factorial(j+i)...
     /(factorial(j-i)*factorial(i)*gamma(alpha+i+1));
 rhoj   = p(j);
 for i=j:-1:1
-    rhojm1 = rhoj*x + p(i-1);
+    rhojm1 = rhoj.*x + p(i-1);
     rhoj = rhojm1;
 end
-p = rhoj*x^alpha;
+p = rhoj.*x.^alpha;
 
 end
 
